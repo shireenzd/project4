b49c80047c87c9e90d96b2d17cd4e9157f28803f
shireenzd <shireenzeineddine@gmail.com>
2023-11-28 10:55:51 +0200

    with-java

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let imageSources = ['images/cover.jpg','images/cover2.jfif','images/cover3.jfif']
-let script = ['Stipy Zig Zag Jigsaw Pillow and Duvet Set','Real Bambo Wall Clock','Brown and Blue Hardbound Book']
+let imageSources = ['images/cover.jpg', 'images/cover2.jfif', 'images/cover3.jfif']
+let script = ['Stipy Zig Zag Jigsaw Pillow and Duvet Set', 'Real Bambo Wall Clock', 'Brown and Blue Hardbound Book']
 
 
 let imgIndex = 0;
@@ -9,12 +9,12 @@ updateUI();
 function moveRight() {
     // move pointer one step to the right
     imgIndex = imgIndex + 1;
-    if(imgIndex>imageSources.length - 1){
-        imgIndex= 0;
+    if (imgIndex > imageSources.length - 1) {
+        imgIndex = 0;
     }
     scriptIndex = scriptIndex + 1;
-    if(scriptIndex>script.length - 1){
-        scriptIndex= 0;
+    if (scriptIndex > script.length - 1) {
+        scriptIndex = 0;
     }
     updateUI();
 }
@@ -22,14 +22,14 @@ function moveRight() {
 
 function moveLeft() {
     imgIndex = imgIndex - 1;
-    if(imgIndex<0){
-        imgIndex= imageSources.length - 1;
+    if (imgIndex < 0) {
+        imgIndex = imageSources.length - 1;
     }
     scriptIndex = scriptIndex - 1;
-    if(scriptIndex<0){
-        scriptIndex= script.length - 1;
+    if (scriptIndex < 0) {
+        scriptIndex = script.length - 1;
     }
-    
+
     updateUI();
 }
 
@@ -39,40 +39,71 @@ function updateUI() {
 }
 
 let leftBtn = document.querySelector('#button1')
-leftBtn.addEventListener('click', moveLeft )
+leftBtn.addEventListener('click', moveLeft)
 
 let rightBtn = document.querySelector('#button2')
-rightBtn.addEventListener('click',  moveRight )
+rightBtn.addEventListener('click', moveRight)
 
-// let script = ['Stipy Zig Zag Jigsaw Pillow and Duvet Set','Real Bambo Wall Clock','Brown and Blue Hardbound Book']
+function toggleColor(element) {
+    element.classList.toggle('clicked');
+}
 
-// let scriptIndex = 0;
-// updateUI2();
 
-// function moveRight() {
-//     // move pointer one step to the right
-//     imgIndex = imgIndex + 1;
-//     if(scriptIndex=script.length - 1){
-//         scriptIndex= 0;
-//     }
-//     updateUI2();
+//or..
+// const hearts = document.querySelectorAll('.fa-heart')
+// function addEventHandler (HTMLElement){
+//     HTMLElement.addEventListener('click', function(){
+//         HTMLElement.classList.toggle('clicked')
+//     })
 // }
+// hearts.forEach(addEventHandler)
 
+ // store search hide/show
+      // get search element
+      let storeSearch = document.querySelector('#store-search');
+      // get search icon
+      let searchIcon = document.querySelector('.fa-magnifying-glass');
+      function showSearch() {
+        storeSearch.classList.remove('hidden');
+      }
+      searchIcon.addEventListener('click', showSearch);
 
-// function moveLeft() {
-//     scriptIndex = scriptIndex - 1;
-//     if(scriptIndex<0){
-//         scriptIndex= script.length - 1;
-//     }
-//     updateUI2();
-// }
+      let closeSearchIcon = document.querySelector('.fa-circle-xmark');
+      function hideSearch() {
+        storeSearch.classList.add('hidden');
+      }
+      closeSearchIcon.addEventListener('click', hideSearch);
+      
+      function filterStoreItems() {
+        let searchValue = searchInput.value;
+        let maxPrice = priceLimitElement.value;
+        if (isNaN(maxPrice)) {
+          maxPrice = 0;
+        }
 
-// function updateUI2() {
-//     document.querySelector('#cover-image .text').textContent = script[scriptIndex];
-// }
+        // loop over all cards
+        cards.forEach(function (element) {
+          const cardInnerText = element.innerText;
+          const cardInnerTextParts = cardInnerText.split('$');
+          const cardPrice = cardInnerTextParts[1];
+          // above is equivalent to const cardPrice = element.innerText.split('$')[1]
 
-// let leftButton = document.querySelector('#button1')
-// leftButton.addEventListener('click', moveLeft )
-
-// let rightButton = document.querySelector('#button2')
-// rightButton.addEventListener('click',  moveRight )
+          // either add or remove class hidden
+          // if card text content matches search value, remove class hidden
+          // hide the ones that are not matching search
+          // & show the matching ones
+          if (element.innerText.toLowerCase().includes(searchValue.toLowerCase()) && (0 == maxPrice || cardPrice < maxPrice)) {
+            console.log('entered block to show card', cardPrice, maxPrice);
+            element.classList.remove('hidden');
+          } else {
+            element.classList.add('hidden');
+          }
+        });
+      }
+      const cards = document.querySelectorAll('.card');
+      // store search filtering
+      let searchInput = document.querySelector('#store-search input[type=text]');
+      searchInput.addEventListener('input', filterStoreItems);
+      // get reference of the price limit input
+      const priceLimitElement = document.querySelector('#price-limit');
+      priceLimitElement.addEventListener('change', filterStoreItems);
